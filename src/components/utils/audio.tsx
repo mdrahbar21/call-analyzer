5a9e22f86048908457b53b1fe19334feee081edf
Md Rahbar <mdrahbar21@iitk.ac.in>
2024-07-13 18:04:52 +0530

    sheet updates

--- src/components/utils/audio.tsx
+++ src/components/utils/audio.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useRef } from 'react';
 
 const SpeakerControl = () => {
-  // Initialize the volume state (0.5 is 50% volume)
 const [volume, setVolume] = useState(0.5);
 
-// Reference to the audio element
 const audioRef = useRef<HTMLAudioElement>(null);
 
-// Function to update volume from the slider
 const handleVolumeChange = (event:any) => {
     const newVolume = parseFloat(event.target.value);
     setVolume(newVolume);
